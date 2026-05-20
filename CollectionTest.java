dcf8e1ae29ac58d66d10d48e4836bd31497464f0
bonastiven09-create <bonastiven09@gmail.com>
2026-05-20 09:31:13 +0700

    pertemuan 9 PBO

---
+++ CollectionTest.java
@@ -0,0 +1,8 @@
+public class CollectionTest {
+    public static void main(String[] args) {
+        
+        System.out.println("Sistem Siap! Mari kita panggil Collections!");
+        // Ketik kodenya di halaman selanjutnya di dalam main() ini!
+
+    }
+}
